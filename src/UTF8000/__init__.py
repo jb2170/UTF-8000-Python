276a9f1058a1e6de2324e3f04ce092c8d97996cc
jb2170 <email@jb2170.com>
2026-05-15 09:44:03 +0100

    Rename files: `encode` and `decode`

--- src/UTF8000/__init__.py
+++ src/UTF8000/__init__.py
@@ -2,6 +2,7 @@
 
 __version__ = "1.0.0"
 
-from .UTF8000Byte               import UTF8000Byte
-from .UTF8000Int                import UTF8000Int
-from .UTF8000IncrementalDecoder import UTF8000IncrementalDecoder
+from .UTF8000Byte import UTF8000Byte
+from .UTF8000Int  import UTF8000Int
+from .decode      import UTF8000IncrementalDecoder
+from .encode      import encode, fancy_encode
