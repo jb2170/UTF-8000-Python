ac76e13cf81dc8a025cceba0385392f5b587c0e8
jb2170 <email@jb2170.com>
2026-06-29 22:06:34 +0100

    Encoder: Add new, better encoder
    Instead of chunking the content bits into a separate list, then copying
    them into a new one with appropriate prefixes attached, just use the one
    list, and 'layer-up' the bits.
    
    We start with the content bits, then add the self-punctuation bits
    (start bits), then add the self-synchronization bits. I thought of doing
    the self-punc and self-sync in one pass through the array, but the first
    byte would have special cases. In C since we'll probably be using
    sensible limits, eg the 51-byte 256-bit encoder, using 64 bytes for a
    cacheline, making multiple passes through the array won't have cache
    penalties. If we go unlimited, say using malloced memory, then this
    doesn't matter too much either. I think this may be premature
    optimization anyways.
    
    The old encoder added the start bits in a way such that the final start
    byte may or may not `pop` the uppermost content bits from the content
    array, and copy them over. To document this I left a yuuuuge (but
    legible and very well written, covering all the cases) comment in the
    fancy encoder.
    
    Also another comment in the fancy encoder about:
    
    > # The 'uppermost' bits from the final extraction will fit into the final start
    > # byte that may have has less than 6 bits of content
    
    was incorrect. e.g.
    
    U+1FFFF has 17 (6 + 6 + 5) content bits and is
    11110000 10011111 10111111 10111111, with the 5 bits all being
    stored in a continuation byte, not the final start byte.

--- src/UTF8000/encode.py
+++ src/UTF8000/encode.py
@@ -1,10 +1,11 @@
 from .UTF8000Byte import (
+    MULTIBYTE_SELF_SYNC_BITS_FIRST,
     MULTIBYTE_SELF_SYNC_BITS_CONTINUATION,
-    MULTIBYTE_PROGRAMMABLE_MASK,
     MULTIBYTE_PROGRAMMABLE_N_BITS,
-    MULTIBYTE_FILLED_FIRST,
-    MULTIBYTE_FILLED_CONTINUATION,
-    ceil_div, fill_n_bits_shifted_by_m
+    MULTIBYTE_PROGRAMMABLE_MASK,
+    MULTIBYTE_SELF_PUNCTUATION_ONES_FULL,
+    MULTIBYTE_SELF_PUNCTUATION_ONES_SOME,
+    ceil_div
 )
 
 def encode(x: int, signed: bool = False) -> bytes:
@@ -21,28 +22,46 @@ def encode(x: int, signed: bool = False) -> bytes:
     ret_ints: list[int] = []
 
     if x < 0x80:
-        ret_ints.append(x)
+        ret_ints.insert(0, x)
 
         return bytes(ret_ints)
 
-    contents: list[int] = []
     n_bits_content_total: int = 0
     y: int = x
 
     while y > MULTIBYTE_PROGRAMMABLE_MASK:
         final_6_bits = y & MULTIBYTE_PROGRAMMABLE_MASK
-        contents.insert(0, final_6_bits)
+        ret_ints.insert(0, final_6_bits)
         n_bits_content_total += MULTIBYTE_PROGRAMMABLE_N_BITS
         y >>= MULTIBYTE_PROGRAMMABLE_N_BITS
 
     final_6_bits = y
-    contents.insert(0, final_6_bits)
+    ret_ints.insert(0, final_6_bits)
     while y > 0:
         n_bits_content_total += 1
         y >>= 1
 
     n_utf_8000_bytes_needed = ceil_div(n_bits_content_total - 1, 5)
 
-    raise NotImplementedError
+    ret_ints = [0 for _ in range(n_utf_8000_bytes_needed - len(ret_ints))] + ret_ints
+
+    idx_self_punctuation = 0
+
+    n_start_bytes_filled_with_ones, n_ones_in_final_start_byte = divmod(n_utf_8000_bytes_needed - 2, MULTIBYTE_PROGRAMMABLE_N_BITS)
+
+    while idx_self_punctuation < n_start_bytes_filled_with_ones:
+        ret_ints[idx_self_punctuation] |= MULTIBYTE_SELF_PUNCTUATION_ONES_FULL
+        idx_self_punctuation += 1
+
+    ret_ints[idx_self_punctuation] |= MULTIBYTE_SELF_PUNCTUATION_ONES_SOME[n_ones_in_final_start_byte]
+
+    idx_self_sync = 0
+
+    ret_ints[idx_self_sync] |= MULTIBYTE_SELF_SYNC_BITS_FIRST
+    idx_self_sync += 1
+
+    while idx_self_sync < n_utf_8000_bytes_needed:
+        ret_ints[idx_self_sync] |= MULTIBYTE_SELF_SYNC_BITS_CONTINUATION
+        idx_self_sync += 1
 
     return bytes(ret_ints)
