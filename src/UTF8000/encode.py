a73b13f23e90e91f7ec83d5aa76432c045d4f012
jb2170 <email@jb2170.com>
2026-06-30 03:19:50 +0100

    Encoder: One return statement at the end of the function

--- src/UTF8000/encode.py
+++ src/UTF8000/encode.py
@@ -23,45 +23,43 @@ def encode(x: int, signed: bool = False) -> bytes:
 
     if x < 0x80:
         ret_ints.insert(0, x)
+    else:
+        n_bits_content_occupied: int = 0
+        y: int = x
 
-        return bytes(ret_ints)
+        while y > MULTIBYTE_PROGRAMMABLE_MASK:
+            final_6_bits = y & MULTIBYTE_PROGRAMMABLE_MASK
+            ret_ints.insert(0, final_6_bits)
+            n_bits_content_occupied += MULTIBYTE_PROGRAMMABLE_N_BITS
+            y >>= MULTIBYTE_PROGRAMMABLE_N_BITS
 
-    n_bits_content_occupied: int = 0
-    y: int = x
-
-    while y > MULTIBYTE_PROGRAMMABLE_MASK:
-        final_6_bits = y & MULTIBYTE_PROGRAMMABLE_MASK
+        final_6_bits = y
         ret_ints.insert(0, final_6_bits)
-        n_bits_content_occupied += MULTIBYTE_PROGRAMMABLE_N_BITS
-        y >>= MULTIBYTE_PROGRAMMABLE_N_BITS
+        while y > 0:
+            n_bits_content_occupied += 1
+            y >>= 1
 
-    final_6_bits = y
-    ret_ints.insert(0, final_6_bits)
-    while y > 0:
-        n_bits_content_occupied += 1
-        y >>= 1
+        n_utf_8000_bytes_needed = ceil_div(n_bits_content_occupied - 1, 5)
 
-    n_utf_8000_bytes_needed = ceil_div(n_bits_content_occupied - 1, 5)
+        ret_ints = [0 for _ in range(n_utf_8000_bytes_needed - len(ret_ints))] + ret_ints
 
-    ret_ints = [0 for _ in range(n_utf_8000_bytes_needed - len(ret_ints))] + ret_ints
+        idx_self_punctuation = 0
 
-    idx_self_punctuation = 0
+        n_start_bytes_filled_with_ones, n_ones_in_final_start_byte = divmod(n_utf_8000_bytes_needed - 2, MULTIBYTE_PROGRAMMABLE_N_BITS)
 
-    n_start_bytes_filled_with_ones, n_ones_in_final_start_byte = divmod(n_utf_8000_bytes_needed - 2, MULTIBYTE_PROGRAMMABLE_N_BITS)
+        while idx_self_punctuation < n_start_bytes_filled_with_ones:
+            ret_ints[idx_self_punctuation] |= MULTIBYTE_SELF_PUNCTUATION_ONES_FULL
+            idx_self_punctuation += 1
 
-    while idx_self_punctuation < n_start_bytes_filled_with_ones:
-        ret_ints[idx_self_punctuation] |= MULTIBYTE_SELF_PUNCTUATION_ONES_FULL
-        idx_self_punctuation += 1
+        ret_ints[idx_self_punctuation] |= MULTIBYTE_SELF_PUNCTUATION_ONES_SOME[n_ones_in_final_start_byte]
 
-    ret_ints[idx_self_punctuation] |= MULTIBYTE_SELF_PUNCTUATION_ONES_SOME[n_ones_in_final_start_byte]
+        idx_self_sync = 0
 
-    idx_self_sync = 0
-
-    ret_ints[idx_self_sync] |= MULTIBYTE_SELF_SYNC_BITS_FIRST
-    idx_self_sync += 1
-
-    while idx_self_sync < n_utf_8000_bytes_needed:
-        ret_ints[idx_self_sync] |= MULTIBYTE_SELF_SYNC_BITS_CONTINUATION
+        ret_ints[idx_self_sync] |= MULTIBYTE_SELF_SYNC_BITS_FIRST
         idx_self_sync += 1
+
+        while idx_self_sync < n_utf_8000_bytes_needed:
+            ret_ints[idx_self_sync] |= MULTIBYTE_SELF_SYNC_BITS_CONTINUATION
+            idx_self_sync += 1
 
     return bytes(ret_ints)
